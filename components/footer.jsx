efcaa3d51f4f85883849ca906e14ff9aee4f4203
Hassan Berro <hasan.berro20@gmail.com>
2026-03-22 21:58:33 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,8 +6,8 @@ import Link from 'next/link';
 export default function Footer({ text, href }) {
     return (
         <footer className="flex flex-row gap-4 p-4 items-center justify-center  py-10 bg-michiganBlue">
-            <div>Made by Hassan Berro, <Link className="underline" target="_blank" href='https://www.andrewye.net/'>Andrew Ye </Link> , and <Link className="underline" target="_blank" href='https://jayshaver.com'>Jay Shaver
-            </Link></div>
+            <div>Made by Hassan Berro
+            </div>
             <div className="flex gap-2">
                 <Link href='https://www.linkedin.com/in/hassanberro/'>
                     <IoLogoLinkedin className="text-3xl" />
